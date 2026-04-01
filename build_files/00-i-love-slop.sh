b54c49a468f00181342bbfe6a3f74ebf37ca1ced
illy <illy@ffi.lol>
2026-04-01 15:19:22 +0100

    i forgor

--- build_files/00-i-love-slop.sh
+++ build_files/00-i-love-slop.sh
@@ -48,7 +48,7 @@ trap 'dnf config-manager setopt keepcache=0' EXIT
 )
 
 ( # install system-config-printer (only libs and udev by default for some reason???
-    dnf -y install system-config-printer-applet
+    dnf -y install system-config-printer system-config-printer-applet
 )
 
 ( # install nm-connection-editor
